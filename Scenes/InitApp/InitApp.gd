80c096f9bcb873ba43e8a012c055255f0b03b7a1
Maaack <marek.belski@gmail.com>
2022-09-11 00:24:06 -0700

    Vines now grow.

--- Scenes/InitApp/InitApp.gd
+++ Scenes/InitApp/InitApp.gd
@@ -5,3 +5,4 @@ export(String, FILE, "*.tscn") var next_scene : String
 func _ready():
 	AppSettings.initialize_from_config()
 	SceneLoader.load_scene(next_scene)
+	randomize()
--- Scenes/Prototype/Level.gd
+++ Scenes/Prototype/Level.gd
@@ -17,7 +17,7 @@ func _vines_make_nutrients():
 			nutrient_instance.position = $Vines.map_to_world(cell_position) + ($Vines.cell_size / 2)
 			$Nutrients.add_child(nutrient_instance)
 
-func _move_nutrient_along_vine_to_flower(nutrient : Node2D):
+func _move_nutrient_along_vine_to_flower(nutrient : Node2D, delay : float):
 	var start_cell = (nutrient.global_position - (vines.cell_size / 2)).floor()
 	var target_cell = ((flower.position - (vines.cell_size / 2)) / vines.cell_size).floor() * vines.cell_size
 	var path_points = vines.get_astar_path_avoiding_obstacles(start_cell, target_cell)
@@ -25,16 +25,63 @@ func _move_nutrient_along_vine_to_flower(nutrient : Node2D):
 		return
 	var path_point = path_points[1] + (vines.cell_size / 2)
 	var tween = get_tree().create_tween()
-	tween.tween_property(nutrient, "position", path_point, 5.0)
+	tween.tween_property(nutrient, "position", path_point, delay)
 
-func _vines_move_nutrients():
+func _vines_move_nutrients(delay : float):
 	for nutrient in $Nutrients.get_children():
-		_move_nutrient_along_vine_to_flower(nutrient)
+		_move_nutrient_along_vine_to_flower(nutrient, delay)
 
-func _plants_take_turn():
+func _filter_values_greater_than(dict : Dictionary, max_value : int) -> Dictionary:
+	var return_dict : Dictionary = {}
+	for key in dict:
+		if dict[key] <= max_value:
+			return_dict[key] = dict[key]
+	return return_dict
+
+func _filter_negative_vectors(vectors : Array) -> Array:
+	var return_vectors : Array = []
+	for vector in vectors:
+		if vector is Vector2 and vector.x >= 0 and vector.y >= 0:
+			return_vectors.append(vector)
+	return return_vectors
+
+func _get_growable_cells():
+	var neighboring_directions : Array = [
+		Vector2.UP,
+		Vector2.DOWN,
+		Vector2.LEFT,
+		Vector2.RIGHT
+	]
+	var neighboring_cells : Dictionary = {}
+	for cell_position in $Vines.get_used_cells():
+		for direction in neighboring_directions:
+			var neighboring_cell = cell_position + direction
+			if $Vines.get_cellv(neighboring_cell) > -1:
+				continue
+			if not neighboring_cell in neighboring_cells:
+				neighboring_cells[neighboring_cell] = 0
+			neighboring_cells[neighboring_cell] += 1
+	neighboring_cells = _filter_values_greater_than(neighboring_cells, 1)
+	return neighboring_cells.keys()
+
+func _grow_vine(cellv : Vector2):
+	$Vines.set_cellv(cellv, 1)
+	$Vines.update_bitmask_area(cellv)
+	$Vines.update()
+
+func _vines_grow(grow_count : int):
+	for i in range(grow_count):
+		var optional_cells : Array = _get_growable_cells()
+		optional_cells = _filter_negative_vectors(optional_cells)
+		if optional_cells.size() == 0:
+			return
+		optional_cells.shuffle()
+		_grow_vine(optional_cells.pop_front())
+
+func _on_HarvetTimer_timeout():
 	_vines_make_nutrients()
-	_vines_move_nutrients()
+	_vines_grow(2)
 
+func _on_TransportTimer_timeout():
+	_vines_move_nutrients($TransportTimer.wait_time - 0.01)
 
-func _on_Timer_timeout():
-	_plants_take_turn()
